9729bc884147f550411ea3596014d31895df73b8
nicolascagna96 <nicolascagna@hotmail.com>
2023-01-25 18:40:49 +0000

    added forms, admin and signals

--- checkout/models.py
+++ checkout/models.py
@@ -63,8 +63,6 @@ class OrderLineItem(models.Model):
         Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
     product = models.ForeignKey(
         Product, null=False, blank=False, on_delete=models.CASCADE)
-    product_size = models.CharField(
-        max_length=2, null=True, blank=True)
     quantity = models.IntegerField(
         null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(
