bd3f34ec832367187a7972a5aa83bad08dccd248
nicolascagna96 <nicolascagna@hotmail.com>
2023-01-25 20:13:05 +0000

    added checkout views and templates

--- fit_zone/settings.py
+++ fit_zone/settings.py
@@ -61,6 +61,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'fit_zone.urls'
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -75,8 +77,13 @@ TEMPLATES = [
                 'django.template.context_processors.request', # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
