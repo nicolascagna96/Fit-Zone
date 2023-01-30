056fbaf77a3b10baf18c722da9db3ef7f16a680d
nicolascagna96 <nicolascagna@hotmail.com>
2023-01-30 09:39:28 +0000

    added fixtures to the project

--- fit_zone/settings.py
+++ fit_zone/settings.py
@@ -239,4 +239,3 @@ else:
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_EMAIL = os.environ.get('EMAIL_HOST_USER')
-
