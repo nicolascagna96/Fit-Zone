7968e57dd0615f635d53ed3e7a652e9e3a4eb1f9
nicolascagna96 <nicolascagna@hotmail.com>
2023-01-30 19:56:57 +0000

    added sitemap and robots files

--- fit_zone/settings.py
+++ fit_zone/settings.py
@@ -28,9 +28,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'DEVELOPMENT' in os.environ
-# DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = False
 
 ALLOWED_HOSTS = ['fit-zone.herokuapp.com', 'localhost']
 
