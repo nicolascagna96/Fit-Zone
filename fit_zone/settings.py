808e5ee9459071ab8acdeb9b47e559b8032e947a
nicolascagna96 <nicolascagna@hotmail.com>
2023-01-30 02:41:34 +0000

    updates settings file

--- fit_zone/settings.py
+++ fit_zone/settings.py
@@ -23,13 +23,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-sv4*w-0lv2h!$%xtu2vs4ppy$lal4hwf2w$3k8ch!#$gu(pq!$"
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = False
 
 ALLOWED_HOSTS = ['fit-zone.herokuapp.com', 'localhost']
 
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 # Application definition
 
