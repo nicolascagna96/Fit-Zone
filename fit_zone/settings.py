0f59344589bbc297c3263684383d2c0472734e44
nicolascagna96 <nicolascagna@hotmail.com>
2023-01-30 13:40:26 +0000

    setting debug to false

--- fit_zone/settings.py
+++ fit_zone/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = False
 
 ALLOWED_HOSTS = ['fit-zone.herokuapp.com', 'localhost']
 
