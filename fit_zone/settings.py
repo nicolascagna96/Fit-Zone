8d4dea93d27126a41262c722d5df17136802ce36
nicolascagna96 <nicolascagna@hotmail.com>
2023-01-31 09:06:17 +0000

    adding static_root for deployment

--- fit_zone/settings.py
+++ fit_zone/settings.py
@@ -188,6 +188,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 STATIC_URL = "/static/"
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
