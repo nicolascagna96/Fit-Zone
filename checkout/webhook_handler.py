f79e00ce23a5cfb5ffc67f099b67794bc846e034
nicolascagna96 <nicolascagna@hotmail.com>
2023-02-06 15:19:22 +0000

    updated webhook handler

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -86,7 +86,7 @@ class StripeWH_Handler:
             try:
                 order = Order.objects.get(
                     full_name__iexact=shipping_details.name,
-                    # email__iexact=billing_details.email,
+                    email__iexact=billing_details.email,
                     # phone_number__iexact=shipping_details.phone,
                     # country__iexact=shipping_details.address.country,
                     # postcode__iexact=shipping_details.address.postal_code,
