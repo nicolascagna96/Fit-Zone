c56773b5ba2fcbc65fee1f59f6aed04f03fee878
nicolascagna96 <nicolascagna@hotmail.com>
2023-02-06 15:03:10 +0000

    updated the webhook handler

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -65,7 +65,7 @@ class StripeWH_Handler:
             if value == "":
                 shipping_details.address[field] = None
 
-          # Update profile information if save_info was checked
+        # Update profile information if save_info was checked
         profile = None
         username = intent.metadata.username
         if username != 'AnonymousUser':
@@ -86,17 +86,17 @@ class StripeWH_Handler:
             try:
                 order = Order.objects.get(
                     full_name__iexact=shipping_details.name,
-                    email__iexact=billing_details.email,
-                    phone_number__iexact=shipping_details.phone,
-                    country__iexact=shipping_details.address.country,
-                    postcode__iexact=shipping_details.address.postal_code,
-                    town_or_city__iexact=shipping_details.address.city,
-                    street_address1__iexact=shipping_details.address.line1,
-                    street_address2__iexact=shipping_details.address.line2,
-                    county__iexact=shipping_details.address.state,
-                    grand_total=grand_total,
-                    original_bag=bag,
-                    stripe_pid=pid,
+                    # email__iexact=billing_details.email,
+                    # phone_number__iexact=shipping_details.phone,
+                    # country__iexact=shipping_details.address.country,
+                    # postcode__iexact=shipping_details.address.postal_code,
+                    # town_or_city__iexact=shipping_details.address.city,
+                    # street_address1__iexact=shipping_details.address.line1,
+                    # street_address2__iexact=shipping_details.address.line2,
+                    # county__iexact=shipping_details.address.state,
+                    # grand_total=grand_total,
+                    # original_bag=bag,
+                    # stripe_pid=pid,
                 )
                 order_exists = True
                 break
