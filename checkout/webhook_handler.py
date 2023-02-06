f8011d8272a228fcfb6966fd9757f947c11b2ec3
nicolascagna96 <nicolascagna@hotmail.com>
2023-02-06 17:28:48 +0000

    fixing bugs during checkout

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -120,7 +120,7 @@ class StripeWH_Handler:
                     street_address2=shipping_details.address.line2,
                     county=shipping_details.address.state,
                     original_bag=bag,
-                    stripe_pid=pid,
+                    # stripe_pid=pid,
                 )
                 for item_id, item_data in json.loads(bag).items():
                     product = Product.objects.get(id=item_id)
